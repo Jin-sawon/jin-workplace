e48e84f356c6d61b3fee327d3706227e5a20ade7
Jin-sawon <cat0879@naver.com>
2022-12-07 04:47:00 +0900

    practice sample RE

--- lib(Firebase Prac)/main.dart
+++ lib(Firebase Prac)/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '파이어베이스) 연습',
+      title: '연습',
       theme: ThemeData(
       ),
       home: Scaffold(
         appBar: AppBar(
-            title: const Text('파이어 베이스 연습')
+            title: const Text('파이어베이스')
 
         ),
         body: const Center(
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
               "https://thx.sfo2.cdn.digitaloceanspaces.com/wr/coverimages/m_11/%EC%98%88%ED%96%89%EC%97%B0%EC%8A%B5_11.jpg")),
 
         ),
-        //const Center(child: Text('한국어도 되니?'))
-      ),
-    );
+           //const Center(child: Text('한국어도 되니?'))
+        ),
+       );
   }
 }
