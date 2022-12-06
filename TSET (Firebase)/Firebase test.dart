6f8a86372165f097050eb71f14c3dd4d3092f43a
Jin-sawon <cat0879@naver.com>
2022-12-07 04:41:53 +0900

    Make 1st practice sample
    thx

--- TSET (Firebase)/Firebase test.dart
+++ TSET (Firebase)/Firebase test.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '연습',
+      title: '파이어베이스) 연습',
       theme: ThemeData(
       ),
       home: Scaffold(
         appBar: AppBar(
-            title: const Text('저는 연습용 샘플입니다.')
+            title: const Text('파이어 베이스 연습')
 
         ),
         body: const Center(
@@ -24,8 +23,8 @@ class MyApp extends StatelessWidget {
               "https://thx.sfo2.cdn.digitaloceanspaces.com/wr/coverimages/m_11/%EC%98%88%ED%96%89%EC%97%B0%EC%8A%B5_11.jpg")),
 
         ),
-           //const Center(child: Text('한국어도 되니?'))
-        ),
-       );
+        //const Center(child: Text('한국어도 되니?'))
+      ),
+    );
   }
 }
